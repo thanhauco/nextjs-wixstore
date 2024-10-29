0862ac8e97d01435c1b20f79aaba3b885deb4541
Thanh Vu <jordan_mbp@Thanhs-MacBook-Pro.local>
2024-10-29 04:14:22 -0700

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Next.js 15 Wix Headless E-Commerce Store
+
+A beautiful and fast e-commerce website built with **Next.js 15** and **Wix Studio Headless APIs**.
+
+Includes everything an online store needs, like cart management, checkout, user accounts, email automations, and payment provider integration.
+
+Uses Next.js best practices like server-side rendering, URL state, optimistic updates, and more.
+
+
---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import { Lora } from "next/font/google";
+import Footer from "./Footer";
+import "./globals.css";
+import Navbar from "./Navbar";
+import ReactQueryProvider from "./ReactQueryProvider";
+
+const lora = Lora({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Flow Shop",
+    absolute: "Flow Shop",
+  },
+  description: "A full-stack e-commerce application built with Next.js 15",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={lora.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem={true}
+          disableTransitionOnChange
+        >
+          <ReactQueryProvider>
+            <Navbar />
+            <div className="min-h-[50vh]">{children}</div>
+            <Footer />
+          </ReactQueryProvider>
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
